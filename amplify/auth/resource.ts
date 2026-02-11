94db37fbc36ecb71b7729e26bab6820de8d1cb6a
Samwell <dizzygokartman@gmail.com>
2026-02-10 19:04:43 -0500

    the notes app

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,17 +1,7 @@
-import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
-const schema = a.schema({  
-Note: a
-    .model({
-      name:a.string(),
-      description: a.string(),
-      image: a.string(),
-    })
-    .authorization((allow) => [allow.owner()]),
-});
-export type Schema = ClientSchema<typeof schema>;
-export const data = defineData({
-  schema,
-  authorizationModes: {
-    defaultAuthorizationMode: 'userPool',
+import { defineAuth } from '@aws-amplify/backend';
+
+export const auth = defineAuth({
+  loginWith: {
+    email: true,
   },
 });
